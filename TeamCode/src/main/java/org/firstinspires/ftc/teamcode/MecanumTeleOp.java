4e9eec8f66158dc366932ca643e90e82244f4fb3
technology08 <cjespenshade@yahoo.com>
2017-10-21 10:50:28 -0400

    Update.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.logitech.LogitechGamepadF310;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by connorespenshade on 10/12/17.
@@ -18,22 +20,39 @@ public class MecanumTeleOp extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        init();
+
+        initMotors();
+
         waitForStart();
 
         while (opModeIsActive()) {
 
-
+            //RIGHT STICK TURNS
+            if (gamepad1.right_stick_x > 0) {
+                rightFront.setPower(gamepad1.right_stick_x);
+                rightBack.setPower(gamepad1.right_stick_x);
+                leftFront.setPower(-gamepad1.right_stick_x);
+                leftBack.setPower(-gamepad1.right_stick_x);
+            } else if (gamepad2.right_stick_x < 0) {
+                rightFront.setPower(-gamepad1.right_stick_x);
+                rightBack.setPower(-gamepad1.right_stick_x);
+                leftFront.setPower(gamepad1.right_stick_x);
+                leftBack.setPower(gamepad1.right_stick_x);
+            }
 
             idle();
         }
     }
 
     private void initMotors() {
-
         leftFront = hardwareMap.dcMotor.get("leftFront");
         leftBack = hardwareMap.dcMotor.get("leftBack");
         rightFront = hardwareMap.dcMotor.get("rightFront");
         rightBack = hardwareMap.dcMotor.get("rightBack");
 
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 }
