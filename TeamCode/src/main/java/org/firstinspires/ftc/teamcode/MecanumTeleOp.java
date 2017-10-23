ee74f52fa620cadc672e96575a3069a408a75a40
technology08 <cjespenshade@yahoo.com>
2017-10-23 18:38:27 -0400

    Added mecanum functionality?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -18,6 +18,8 @@ public class MecanumTeleOp extends LinearOpMode {
     private DcMotor rightFront;
     private DcMotor rightBack;
 
+    private int DEADZONE = 20;
+
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -28,7 +30,7 @@ public class MecanumTeleOp extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-
+/*
             //RIGHT STICK TURNS
             if (gamepad1.right_stick_x > 0) {
                 rightFront.setPower(gamepad1.right_stick_x);
@@ -41,18 +43,31 @@ public class MecanumTeleOp extends LinearOpMode {
                 leftFront.setPower(gamepad1.right_stick_x);
                 leftBack.setPower(gamepad1.right_stick_x);
             }
+*/
+            moveMecanum(gamepad1.right_stick_x, gamepad1.left_stick_y, gamepad1.left_stick_x);
 
             idle();
         }
     }
 
     private void initMotors() {
-        leftFront = hardwareMap.dcMotor.get("leftFront");
-        leftBack = hardwareMap.dcMotor.get("leftBack");
-        rightFront = hardwareMap.dcMotor.get("rightFront");
-        rightBack = hardwareMap.dcMotor.get("rightBack");
-
-        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront = hardwareMap.dcMotor.get("lf");
+        leftBack = hardwareMap.dcMotor.get("lb");
+        rightFront = hardwareMap.dcMotor.get("rf");
+        rightBack = hardwareMap.dcMotor.get("rb");
     }
+
+    private void moveMecanum(float Ch1, float Ch3, float Ch4) {
+        double FrontLeft = Ch3 + Ch1 + Ch4;
+        double RearLeft = Ch3 + Ch1 - Ch4;
+        double FrontRight = Ch3 - Ch1 - Ch4;
+        double RearRight = Ch3 - Ch1 + Ch4;
+
+        //ADD DEADZONE
+        leftFront.setPower(java.lang.Math.abs(FrontLeft) > DEADZONE ? FrontLeft : 0);
+        leftBack.setPower(java.lang.Math.abs(RearLeft) > DEADZONE ? RearLeft : 0);
+        rightFront.setPower(java.lang.Math.abs(FrontRight) > DEADZONE ? FrontRight : 0);
+        rightBack.setPower(java.lang.Math.abs(RearRight) > DEADZONE ? RearRight : 0);
+    }
+
 }
