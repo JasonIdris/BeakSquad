649b90a18375e427ea8494d18c2d0b904170cd3c
albusdumbledore <spamwhenever@gmail.com>
2017-11-09 19:55:21 -0500

    فثسفسفثففسثasdasd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 /**
  * Created by albusdumbledore on 11/6/17.
  */
-//slashforceforlifess
+//slashforceforlifesszzzzz
 
 public class Lift extends MasterTeleOp {
     private static Lift instance = new Lift();
