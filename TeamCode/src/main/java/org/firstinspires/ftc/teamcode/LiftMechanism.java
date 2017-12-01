00d08415e2aa51654b7ab01810ae9ec42415416d
albusdumbledore <spamwhenever@gmail.com>
2017-11-30 19:54:35 -0500

    added integration of Rev robotics touch sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftMechanism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftMechanism.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
@@ -23,6 +24,8 @@ public class LiftMechanism extends LinearOpMode {
     private Servo leftServo;
     private Servo rightServo;
     private DcMotor liftMotor;
+
+    DigitalChannel digitalTouch;  // Hardware Device Object
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -78,11 +81,34 @@ public class LiftMechanism extends LinearOpMode {
                 liftMotor.setPower(0);
             }
 
+            // get a reference to our digitalTouch object.
+            digitalTouch = hardwareMap.get(DigitalChannel.class, "sensor_digital");
 
-            idle();
+            // set the digital channel to input.
+            digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+
+            // wait for the start button to be pressed.
+            waitForStart();
+
+            // while the op mode is active, loop and read the light levels.
+            // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
+            while (opModeIsActive()) {
+
+                // send the info back to driver station using telemetry function.
+                // if the digital channel returns true it's HIGH and the button is unpressed.
+                if (digitalTouch.getState() == true) {
+                    telemetry.addData("Digital Touch", "Is Not Pressed");
+                } else {
+                    telemetry.addData("Digital Touch", "Is Pressed");
+                }
+
+                telemetry.update();
+
+
+                idle();
+            }
         }
     }
-
     private void initMotors() {
 
         leftServo = hardwareMap.servo.get("ls");
