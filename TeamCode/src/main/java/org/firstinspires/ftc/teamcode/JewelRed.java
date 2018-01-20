232e2b25617f2f30004d656c4968c6baeaa0762e
technology08 <cjespenshade@yahoo.com>
2018-01-20 10:14:33 -0500

    Some build error.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelRed.java
@@ -23,6 +23,8 @@ public class JewelRed extends LinearOpMode {
     private DcMotor rightFront;
     private DcMotor rightBack;
 
+    private boolean moved = false;
+
     private double[] reds = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
     private double[] blues = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
@@ -40,53 +42,56 @@ public class JewelRed extends LinearOpMode {
 
         while (opModeIsActive()) {
             armServo.setPosition(0.5);
+            while (!moved) {
+                Double red_sum = 0.0;
+                Double blue_sum = 0.0;
+                for (int i = reds.length-1; i > 0; i--) {
+                    reds[i] = reds[i - 1];
+                    red_sum += reds[i];
+                    blues[i] = blues[i - 1];
+                    blue_sum += blues[i];
+                }
 
-            Double red_sum = 0.0;
-            Double blue_sum = 0.0;
-            for (int i = reds.length-1; i > 0; i--) {
-                reds[i] = reds[i - 1];
-                red_sum += reds[i];
-                blues[i] = blues[i - 1];
-                blue_sum += blues[i];
+                telemetry.addData("Red", colorSensor.red());
+                telemetry.addData("Green", colorSensor.green());
+                telemetry.addData("Blue", colorSensor.blue());
+
+                reds[0] = ((double) colorSensor.red());
+                blues[0] = ((double) colorSensor.blue());
+
+                red_sum += reds[0];
+                blue_sum += blues[0];
+                Double red_avg = red_sum/reds.length;
+                Double blue_avg = blue_sum/blues.length;
+                telemetry.addData("RED_AVG", red_avg);
+                telemetry.addData("BLUE_AVG", blue_avg);
+
+                Double identifier = (red_avg-blue_avg) / red_avg;
+
+                if (identifier >= 0.4) {
+                    //Red
+                    activatedColor = ColorType.Red;
+
+                    gamepadDrive(0, 0, 1);
+                    sleep(500);
+                    gamepadDrive(0,0,0);
+                    moved = true;
+
+                } else if (identifier <= -0.15) {
+                    //Blue
+                    activatedColor = ColorType.Blue;
+                    gamepadDrive(0, 0,-1);
+                    sleep(500);
+                    gamepadDrive(0,0,0);
+                    moved = true;
+                } else {
+
+                    activatedColor = ColorType.Unknown;
+                }
+                telemetry.addData("Activated Color", activatedColor);
+                telemetry.update();
             }
-            
-            telemetry.addData("Red", colorSensor.red());
-            telemetry.addData("Green", colorSensor.green());
-            telemetry.addData("Blue", colorSensor.blue());
-            
-            reds[0] = ((double) colorSensor.red());
-            blues[0] = ((double) colorSensor.blue());
 
-            red_sum += reds[0];
-            blue_sum += blues[0];
-            Double red_avg = red_sum/reds.length;
-            Double blue_avg = blue_sum/blues.length;
-            telemetry.addData("RED_AVG", red_avg);
-            telemetry.addData("BLUE_AVG", blue_avg);
-            
-            Double identifier = (red_avg-blue_avg) / red_avg;
-            
-            if (identifier >= 0.4) {
-                //Red
-                activatedColor = ColorType.Red;
-
-                gamepadDrive(0, 0, 1);
-                sleep(500);
-                gamepadDrive(0,0,0);
-
-            } else if (identifier <= 0.15) {
-                //Blue
-                activatedColor = ColorType.Blue;
-                gamepadDrive(0, 0,-1);
-                sleep(500);
-                gamepadDrive(0,0,0);
-
-            } else {
-
-                activatedColor = ColorType.Unknown;
-            }
-            telemetry.addData("Activated Color", activatedColor);
-            telemetry.update();
             
             idle();
         }
